ae3fde8272ee15ad497ab2ec20c26540893fd3ae
rishi <71474438+rishicantcode@users.noreply.github.com>
2025-01-01 11:21:56 -0800

    commit-timestamp-18:46

--- renewableenergyapp/lib/core/configs/theme/app_theme.dart
+++ renewableenergyapp/lib/core/configs/theme/app_theme.dart
@@ -6,9 +6,31 @@ class AppTheme {
   static final lightTheme = ThemeData(
     primaryColor: AppColors.primary,
     scaffoldBackgroundColor: AppColors.lightBackground,
+    brightness: Brightness.light,
+    fontFamily: 'Satoshi',
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
         backgroundColor: AppColors.primary,
+        textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30)
+        )
+      )
+    )
+  );
+
+    static final darkTheme = ThemeData(
+    primaryColor: AppColors.primary,
+    scaffoldBackgroundColor: AppColors.darkBackground,
+    brightness: Brightness.dark,
+    fontFamily: 'Satoshi',
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primary,
+        textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30)
+        )
       )
     )
   );
